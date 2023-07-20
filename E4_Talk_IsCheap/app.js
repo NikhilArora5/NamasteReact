3e81f42dbd003186eca8838709c74282678134b5
Nikhil <mern2@omdata.world>
2023-07-20 16:24:41 +0530

    Ep-4 start

---
+++ E4_Talk_IsCheap/app.js
@@ -0,0 +1,40 @@
+ 
+    import React from "react"
+    import  ReactDOM  from "react-dom/client"
+    
+
+
+
+
+
+
+
+const ReactElement=React.createElement("h1",{},"H1 with React.CreateElement")
+
+/*
+#Header
+    -Logo
+    -Nav items
+#Body
+    -Search Bar
+    -RestaurantContainer
+        --RestaurantCard
+#Footer
+    -Copyright
+    -Links
+    -Adress
+    -Contact
+*/
+
+
+
+
+
+
+
+
+
+
+const root= ReactDOM.createRoot(document.getElementById("root"))
+
+root.render()
