3d6378e3202a5cc3c3ca20c760d781e522d8add4
Nikhil <aroranikhil8184@gmail.com>
2023-07-21 17:34:11 +0530

    intro to props

--- E4_Talk_IsCheap/app.js
+++ E4_Talk_IsCheap/app.js
@@ -36,25 +36,27 @@ const styleObj={
 const Body=()=>{
     return (
         <div className="body"style={   { backgroundColor: "#f0f0f0f"}  } >
-         <div className="search">Searc</div>
+         <div className="search">Search</div>
 
          <div className="res-container">
-           <RestaurantCard/>
+           <RestaurantCard name="KFC" cuisine="North India ,chineese,Asian" rating= "4.3"/>
+           <RestaurantCard name="Brew Estate" cuisine="Mexican ,chineese,Asian" rating= "4.5"/>
          </div>
         </div>
     )
 }
 
-const RestaurantCard=()=>{
-    return (
+const RestaurantCard=(props)=>{
+    const {name,cuisine,rating}=props
+        return (
         <div className="res-card">
             <img 
             className="res-logo"
             alt="res-logo"
             src="https://th.bing.com/th/id/R.5ff9c57dcd7405f38eda737bc5be9e1c?rik=mTQ%2ff61ou%2bKfmA&riu=http%3a%2f%2fwww.wallpaperup.com%2fuploads%2fwallpapers%2f2015%2f04%2f11%2f659101%2f5ff9c57dcd7405f38eda737bc5be9e1c.jpg&ehk=VJlvinFd1DVy3g53p5BuOjf2Nk2ewnvWL1cQ78Ga9uQ%3d&risl=&pid=ImgRaw&r=0" />
-            <h3>Meghana Foods</h3>
-             <h4>North India ,chineese,Asian</h4>
-             <h4>4.5 Stars</h4>
+            <h3>{name}</h3>
+             <h4>{cuisine}</h4>
+             <h4>{rating}</h4>
              <h4>Delivery :35 Minutes</h4>
 
 
