b45199afda82081a3307200efd3f7e78ef117e59
Nikhil <aroranikhil8184@gmail.com>
2023-07-31 10:08:46 +0530

    EP-6 till shimmer ui

---
+++ E5Above/src/components/Shimmer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Shimmer = () => {
+  return (
+    <div className='shimmer-conatiner'>
+
+        <div className='fake-resCard'> </div>
+        <div className='fake-resCard'> </div>
+        <div className='fake-resCard'> </div>
+        <div className='fake-resCard'> </div>
+        <div className='fake-resCard'> </div>
+        <div className='fake-resCard'> </div>
+        <div className='fake-resCard'> </div>
+        <div className='fake-resCard'> </div>
+        <div className='fake-resCard'> </div>
+        <div className='fake-resCard'> </div>
+      
+    </div>
+  )
+}
+
+export default Shimmer
