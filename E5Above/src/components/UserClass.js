0d5fb775157f890c308a99a8c9d043272cd5a478
Nikhil <aroranikhil8184@gmail.com>
2023-08-07 16:55:11 +0530

    creating and updating state

--- E5Above/src/components/UserClass.js
+++ E5Above/src/components/UserClass.js
@@ -7,16 +7,28 @@ class UserClass extends React.Component{
 
         super(props)
 
-        console.log("Receiving Props in classBased",props)
+        // console.log("Receiving Props in classBased",props)
+        this.state={
+            count:0
+        }
     }
 
     render(){
+
+        const {count}=this.state
 
         return (
 
             <div className="user-card">
                 <h3>User Class</h3>
-                <h2>{this.props.name}</h2>
+                <h2>{this.state.name}</h2>
+                
+                <h2>Count :{count}</h2>  <button onClick={()=>{
+                    this.setState({
+                        count:this.state.count+1
+                    })
+                }} >Inc Count</button>
+                
                 
 
             </div>
