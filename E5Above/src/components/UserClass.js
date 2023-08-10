8685ddc12c58a13931ad7e4155e8631086c72874
Nikhil <aroranikhil8184@gmail.com>
2023-08-10 08:06:05 +0530

    comp did update

--- E5Above/src/components/UserClass.js
+++ E5Above/src/components/UserClass.js
@@ -15,28 +15,37 @@ class UserClass extends React.Component{
             }
         }
 
-        //  console.log( this.props.name+"Child Contructour called")
+         console.log( "Contructour called")
     }
 
    async componentDidMount(){
 
-        // console.log(this.props.name+"Child component Did Mount")
+        console.log(" component Did Mount")
         const data= await fetch(" https://api.github.com/users/NikhilArora5")
 
         let json=await data.json()
-        console.log("JSON USER API",json)
+        // console.log("JSON USER API",json)
 
         if(json) this.setState({userInfo:json})
     }
+   
 
+    async componentDidUpdate(){
+        console.log(" component Did update called")
+    }
     render(){
         //  console.log(this.props.name+"Child Rendering  called")
         const {avatar_url,login,html_url}=this.state.userInfo
+        if(!login){
+              console.log("Rendering  called with Dummy Data State>>>",this.state.userInfo)
+        }else{
+            console.log("Rendering  called with Updated Data State>>>",this.state.userInfo)
+        }
 
         return (
 
             <div className="user-card">
-              {/* <img src={avatar_url}/> */}
+              <img src={avatar_url}/>
                 <h2>{login}</h2>
                 <a target="_blank" href={html_url} > Github  </a>
 
