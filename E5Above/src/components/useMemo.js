f7d75a3d93566c2130a8ea54b0864a699832d8fd
Nikhil <aroranikhil8184@gmail.com>
2023-09-11 06:36:59 +0530

    adding useMemo

---
+++ E5Above/src/components/useMemo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const useMemo = () => {
+
+    console.log("------------Rednering----")
+
+  return (
+
+    <div>
+      
+    </div>
+  )
+}
+
+export default useMemo
