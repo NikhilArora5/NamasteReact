55543a21a5e83f96d9fd754231a2c56881d4fa21
Nikhil <aroranikhil8184@gmail.com>
2023-09-13 22:33:37 +0530

    adding basic logout and login

--- E5Above/src/components/Header.js
+++ E5Above/src/components/Header.js
@@ -8,13 +8,20 @@ import userContext from "../utils/context/userContext"
 import { useContext } from "react"
 import {  useSelector } from "react-redux/es/hooks/useSelector"
 import { userLogout } from "../Api/auth"
+import { useNavigate } from "react-router-dom"
+import { clearCart } from "../Redux/slices/cartSlice"
+import { useDispatch } from "react-redux"
+
 // common classes
 let li="mx-4"
 
 
 const Header=()=>{
+  let dispatch=useDispatch()
+  let navigate=useNavigate()
+
     const [loginBtn,setLoginBtn]=useState("Login")
-    const {loggedInUser,isLoggedIn,setIsLoggedIn}=useContext(userContext)
+    const {loggedInUser,isLoggedIn,setIsLoggedIn,}=useContext(userContext)
 
     const cartItems=useSelector((store)=>(store.cart.items))
 
@@ -30,7 +37,21 @@ useEffect(()=>{
 
     const onlineStatus=useOnlineStaus()
     const auth=async()=>{
-        let userLogout()
+        let resData=await userLogout  ()
+        console.log("----------Logout 2222---",resData)
+        console.log("status--",resData.status)
+      if(resData.status==200){
+
+        console.log("updating after logout")
+
+        navigate("/login")
+        dispatch(clearCart())
+        setIsLoggedIn(false)
+        
+      }
+        
+
+
     }
     return ( 
     
@@ -45,16 +66,16 @@ useEffect(()=>{
 
             <li>Online Status :{onlineStatus?"✔️":"🔴"}</li>
               <li className={li}><Link to={"/"} >Home</Link> </li>
-              <li className={li}>  Restaurants  </li>
+              {/* <li className={li}>  Restaurants  </li> */}
               <li className={li}> <Link to={"/about"}>About us</Link> </li>
               <li className={li}> <Link to={"/contact"}>Contact</Link> </li>
-              <li className={li}> <Link to={"/grocery"}>Grocery</Link> </li>
+              {/* <li className={li}> <Link to={"/grocery"}>Grocery</Link> </li> */}
            
-              <li  className= "font-bold">  <Link to={"/cart"}>Cart({cartItems.length})</Link></li>
+              <li  className= "font-bold mx-1">  <Link to={"/cart"}>Cart({cartItems.length})</Link></li>
               {isLoggedIn?<li  className={li}>{loggedInUser}</li>:""}
               <button className="login-Btn" 
-              onClick={()=>(setIsLoggedIn(!isLo))}>
-                <Link to={"/login"}> {isLoggedIn?"Logout":"Login"}</Link>
+              onClick={()=>(isLoggedIn?auth():null)}>
+                <Link to={isLoggedIn?"/":"/login"}> {isLoggedIn?"Logout":"Login"}</Link>
                
                 
                 </button>
--- E5Above/src/components/Login.js
+++ E5Above/src/components/Login.js
@@ -42,7 +42,7 @@ const Login = () => {
      
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       <a className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-          <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"/>
+          {/* <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"/> */}
           Food Cafe   
       </a>
       <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
@@ -52,36 +52,38 @@ const Login = () => {
               </h1>
               <form className="space-y-4 md:space-y-6" >
                   <div>
-                      <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                      <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
                       <input onChange={(e)=>(handleChange(e))} 
                         //  onChange={(e)=>(    setSearchText(e.target.value))}
                       type="email" name="email"  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  required=""/>
                   </div>
                   <div>
                       <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                      <input onChange={(e)=>(handleChange(e))}  type="password" name="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                      <input onChange={(e)=>(handleChange(e))}  type="password" name="password" placeholder="" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
                   </div>
                   <div className="flex items-center justify-between">
                       <div className="flex items-start">
-                          <div className="flex items-center h-5">
+                          {/* <div className="flex items-center h-5">
                             <input  aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
-                          </div>
-                          <div className="ml-3 text-sm">
+                          </div> */}
+                          {/* <div className="ml-3 text-sm">
                             <label  className="text-gray-500 dark:text-gray-300">Remember me</label>
-                          </div>
+                          </div> */}
+                           <div className='text-center'>
+                    <button   className="text-white bg-blue-400 p-2 px-4 rounded-md cursor-pointer mx-2" onClick={(e)=>handleSubmit(e)}> 
+                        Login
+                    </button>
+                  </div>
                       </div>
                       <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                   </div>
+                 
                   <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                       Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
                   </p>
 
-                  <div>
-                    <button onClick={(e)=>handleSubmit(e)}> 
-                        Submit
-                    </button>
-                  </div>
+                 
               </form>
           </div>
       </div>
